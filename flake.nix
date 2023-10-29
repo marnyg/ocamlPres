1946250b316d6c34142a3b521110e0b186e86665
marius <marnyg@proton.me>
2023-10-29 16:21:35 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "My OCaml project";
+
+  outputs = { self, nixpkgs }: rec {
+    devShells.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
+      buildInputs = [
+        ocaml
+        dune_3
+        nixpkgs.legacyPackages.x86_64-linux.opam
+        #gcc
+        coreutils
+        #opam2json
+        #ocamle_make
+        #make
+        libev
+      ];
+      shellHook = '' 
+        opam switch create my_switch2 4.14.1 || opam switch my_switch2
+        eval $(opam env)
+        #opam install . --deps-only
+      '';
+    };
+
+    packages.x86_64-linux.myOcamlPackage = with nixpkgs.legacyPackages.x86_64-linux; ocamlPackages.buildDunePackage rec {
+      pname = "myPro";
+      version = "0.1.0";
+      duneVersion = "3";
+      src = ./.;
+
+      buildInputs = [
+        ocamlPackages.core
+        ocamlPackages.alcotest
+        ocamlPackages.ppx_inline_test
+      ];
+      strictDeps = true;
+
+      useDuneConfig = true; # Use this to read dependencies from .opam file
+      doCheck = true;
+      checkTarget = "test";
+
+      #preBuild = ''
+      #  dune build myPro.opam
+      #'';
+    };
+    packages.x86_64-linux.default = packages.x86_64-linux.myOcamlPackage;
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+  };
+}
+
---
+++ test/myPro.ml
@@ -0,0 +1,4 @@
+
+(* let add a b = a + b *)
+
+(* let%test3 _ = add 5 2 == 7 *)
