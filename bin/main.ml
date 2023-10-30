b48fad35efc3970db1d75b816bfcde1faf15fba8
marius <marnyg@proton.me>
2023-10-30 18:53:38 +0100

    first working html

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,21 @@
-let () = print_endline "Hello, World!"
+let page =
+  let open Dream_html in
+  let open HTML in
+  html [lang "en"] [
+    head [] [
+      title [] "Dream-html" ];
+    body [] [
+      h1 [] [txt "Dream-html"];
+      p [] [txt "Is cool!"];
+      form [method_ `POST; action "/feedback"] [
 
-let hello who =
-  <html>
-  <body>
-    <h1>Hello, <%s who %>!</h1>
-  </body>
-  </html>
+        label [for_ "what-you-think"] [txt "Tell us what you think!"];
+        input [name "what-you-think"; id "what-you-think"];
+        input [type_ "submit"; value "Send"] ] ] ]
 
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
-    Dream.get "/" (fun _ ->
-      Dream.html (hello "world"));
+    Dream.get "/" (fun _ ->(Dream_html.respond page ))
   ]
