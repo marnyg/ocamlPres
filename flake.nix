815f2208de1600fc99ed4356092ee2d0b6e946c4
marius <marnyg@proton.me>
2023-11-05 19:09:17 +0000

    opam2nix

--- flake.nix
+++ flake.nix
@@ -2,112 +2,131 @@
   description = "My OCaml project";
 
   inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
+  # inputs.opam2nix-src = {
+  #     url = "https://github.com/timbertson/opam2nix/archive/v1.tar.gz";
+  #     flake = false;
+  #   };
+  #inputs.opam-nix.url = "github:tweag/opam-nix";
+  inputs.opam2nix-src = { url = "github:dwarfmaster/opam2nix"; };
 
-  outputs =
-    { self, nixpkgs }: rec{
-      #   devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; mkShell  rec {
-      #   
-      #     nativeBuildInputds=[
-      #       ocamlPackages.findlib
-      #       ocaml
-      #       dune_3
-      #     ];
-      #     buildInputs = [
-      #       ocamlPackages.ocaml-lsp
-      #       ocaml
-      #       dune_3
-      #       opam
-      #       openssl
-      #       ocamlformat
-      #       libev
-      #       pkg-config
-      #       ocamlPackages.findlib
-      #       ocamlPackages.core
-      #       ocamlpackages.dream
-      #       ocamlpackages.tyxml
-      #       ocamlPackages.alcotest
-      #       ocamlPackages.ppx_inline_test
-      #       ocamlPackages.ppxlib
-      #       ocamlPackages.base
-      #     ];
-      #       shellHook = ''
-      #   export OCAMLPATH=""
-      #
-      #   addToOCamlPath() {
-      #     local lib_path="$1/lib/ocaml/${ocaml.version}/site-lib"
-      #     if [[ -d "$lib_path" ]]; then
-      #       if [[ -z "$OCAMLPATH" ]]; then
-      #         export OCAMLPATH="$lib_path"
-      #       else
-      #         export OCAMLPATH="$OCAMLPATH:$lib_path"
-      #       fi
-      #     fi
-      #   }
-      #
-      #   for pkg in ${toString buildInputs}; do
-      #     addToOCamlPath $pkg
-      #   done
-      #
-      #   echo "OCAMLPATH is set to $OCAMLPATH"
-      # '';
-      #     # shellHook = '' 
-      #     #   #opam switch create my_switch2 4.14.1 || opam switch my_switch2
-      #     #   #eval $(opam env)
-      #     #   #opam install . --deps-only
-      #     # '';
-      #     LSP_SERVERS="ocamllsp";
-      #   };
-      devShells.x86_64-linux.default =
-        let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
-          ocamlEnvironment = pkgs.buildEnv {
-            name = "ocaml-environment";
-            paths = with pkgs; [
-              ocaml
-              dune_3
-              ocamlformat
-              ocamlPackages.findlib
-              ocamlPackages.core
-              ocamlPackages.dream
-              ocamlPackages.tyxml
-              ocamlPackages.alcotest
-              ocamlPackages.ppx_inline_test
-            ];
-          };
-        in
-        with nixpkgs.legacyPackages.x86_64-linux; mkShell rec {
-          name = "myPro-dev";
-          buildInputs = with ocamlPackages; [ ocamlEnvironment ];
-
-          shellHook = ''
-            export OCAMLPATH="${ocamlEnvironment}/lib/ocaml/${ocaml.version}/site-lib"
-            echo    $OCAMLPATH
-          '';
+  outputs = { self, nixpkgs, opam2nix-src }: rec{
+    #   devshells.x86_64-linux.default = with nixpkgs.legacypackages.x86_64-linux; mkshell  rec {
+    #   
+    #     nativebuildinputds=[
+    #       ocamlpackages.findlib
+    #       ocaml
+    #       dune_3
+    #     ];
+    #     buildinputs = [
+    #       ocamlpackages.ocaml-lsp
+    #       ocaml
+    #       dune_3
+    #       opam
+    #       openssl
+    #       ocamlformat
+    #       libev
+    #       pkg-config
+    #       ocamlpackages.findlib
+    #       ocamlpackages.core
+    #       ocamlpackages.dream
+    #       ocamlpackages.tyxml
+    #       ocamlpackages.alcotest
+    #       ocamlpackages.ppx_inline_test
+    #       ocamlpackages.ppxlib
+    #       ocamlpackages.base
+    #     ];
+    #       shellhook = ''
+    #   export ocamlpath=""
+    #
+    #   addtoocamlpath() {
+    #     local lib_path="$1/lib/ocaml/${ocaml.version}/site-lib"
+    #     if [[ -d "$lib_path" ]]; then
+    #       if [[ -z "$ocamlpath" ]]; then
+    #         export ocamlpath="$lib_path"
+    #       else
+    #         export ocamlpath="$ocamlpath:$lib_path"
+    #       fi
+    #     fi
+    #   }
+    #
+    #   for pkg in ${tostring buildinputs}; do
+    #     addtoocamlpath $pkg
+    #   done
+    #
+    #   echo "ocamlpath is set to $ocamlpath"
+    # '';
+    #     # shellhook = '' 
+    #     #   #opam switch create my_switch2 4.14.1 || opam switch my_switch2
+    #     #   #eval $(opam env)
+    #     #   #opam install . --deps-only
+    #     # '';
+    #     lsp_servers="ocamllsp";
+    #   };
+    a = opam2nix-src;
+    devShells.x86_64-linux.default =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        ocaml = pkgs.ocaml;
+        opam2nix = import opam2nix-src { inherit pkgs; };
+        selection = opam2nix.buildInputs {
+          inherit ocaml;
+          selection = ./opam-selection.nix;
+          src = ./.;
         };
 
-      packages.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; ocamlPackages.buildDunePackage rec {
-        pname = "myPro";
-        version = "0.1.0";
-        duneVersion = "3";
-        src = ./.;
-
-        buildInputs = [
-          ocamlPackages.core
-          ocamlPackages.dream
-          ocamlPackages.tyxml
-          ocamlPackages.alcotest
-          ocamlPackages.ppx_inline_test
+        ocamlEnvironment = pkgs.buildEnv {
+          name = "ocaml-environment";
+          paths = with pkgs; [
+            ocaml
+            dune_3
+            ocamlformat
+            ocamlPackages.findlib
+            ocamlPackages.core
+            ocamlPackages.dream
+            ocamlPackages.tyxml
+            ocamlPackages.alcotest
+            ocamlPackages.ppx_inline_test
+          ];
+          # ] +selection;
+        };
+      in
+      with nixpkgs.legacyPackages.x86_64-linux; mkShell rec {
+        name = "myPro-dev";
+        buildInputs = with ocamlPackages; [
+          ocamlEnvironment
+          opam2nix-src.outputs.defaultPackage.x86_64-linux
+          selection
         ];
 
-        #useDuneConfig = true; # Use this to read dependencies from .opam file
-        doCheck = true;
-        checkTarget = "test";
-
-        #preBuild = ''dune build myPro.opam '';
+        shellHook = ''
+          export OCAMLPATH="${ocamlEnvironment}/lib/ocaml/${ocaml.version}/site-lib"
+          echo    $OCAMLPATH
+          export NIX_PATH=nixpkgs=${nixpkgs}:$NIX_PATH
+        '';
       };
-      apps.x86_64-linux.default = { type = "app"; program = "${packages.x86_64-linux.default}/bin/main"; };
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+
+    packages.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; ocamlPackages.buildDunePackage rec {
+      pname = "myPro";
+      version = "0.1.0";
+      duneVersion = "3";
+      src = ./.;
+
+      buildInputs = [
+        ocamlPackages.core
+        ocamlPackages.dream
+        ocamlPackages.tyxml
+        ocamlPackages.alcotest
+        ocamlPackages.ppx_inline_test
+      ];
+
+      #useDuneConfig = true; # Use this to read dependencies from .opam file
+      doCheck = true;
+      checkTarget = "test";
+
+      #preBuild = ''dune build myPro.opam '';
     };
+    apps.x86_64-linux.default = { type = "app"; program = "${packages.x86_64-linux.default}/bin/main"; };
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+  };
 }
 
