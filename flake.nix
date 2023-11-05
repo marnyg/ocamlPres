f97cba51259f86a9a543e4837b62a10804f55479
marius <marnyg@proton.me>
2023-11-05 20:58:46 +0000

    devshell now works with opam-nix!

--- flake.nix
+++ flake.nix
@@ -5,76 +5,70 @@
   inputs.opam-nix.url = "github:tweag/opam-nix";
 
   outputs = { self, nixpkgs, opam-nix }: rec{
+
+    a=let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+
+        # package = "myPro";
+        on = opam-nix.lib.x86_64-linux;
+        scope = on.buildOpamProject' { } ./. { ocaml-base-compiler = "*"; };
+        filterForDerivations = list: builtins.filter pkgs.lib.isDerivation list;
+        scope' = filterForDerivations (builtins.attrValues scope);
+    in
+     pkgs.buildEnv {
+          name = "ocaml-environment";
+          paths = with pkgs; [ 
+          # ocaml 
+          # dune_3
+          # ocamlformat 
+          # ocamlPackages.findlib 
+        ] ++ scope';
+        };
+
+
+
+
     devShells.x86_64-linux.default =
       let
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
 
-        package = "myPro";
         on = opam-nix.lib.x86_64-linux;
-        scope = on.buildOpamProject { } package ./. { ocaml-base-compiler = "*"; };
-        devPackagesQuery = {
-
-          # You can add "development" packages here. They will get added to the devShell automatically.
-          # ocaml-lsp-server = "*";
-          # ocamlformat = "*";
-        };
-        devPackages = builtins.attrValues
-          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope);
+        scope = on.buildOpamProject' { } ./. { ocaml-base-compiler = "*"; };
+        filterForDerivations = list: builtins.filter pkgs.lib.isDerivation list;
+        scope' = filterForDerivations (builtins.attrValues scope);
 
 
         ocamlEnvironment = pkgs.buildEnv {
           name = "ocaml-environment";
-          paths = with pkgs; [
-            ocaml
-            dune_3
-            ocamlformat
-            ocamlPackages.findlib
-            # ocamlPackages.core
-            # ocamlPackages.dream
-            # ocamlPackages.tyxml
-            # ocamlPackages.alcotest
-            # ocamlPackages.ppx_inline_test
-            # ];
-          ] ++ devPackages;
+          # paths = with pkgs; [ 
+          # # ocamlPackages.findlib 
+          # ] ++ scope';
+
+          paths = scope';
         };
+
       in
       with nixpkgs.legacyPackages.x86_64-linux; mkShell rec {
-        name = "myPro-dev";
-        buildInputs = with ocamlPackages; [
-          ocamlEnvironment
-        ];
+        # inputsFrom = [ scope.myPro ];
+        buildInputs = with ocamlPackages; [ ocamlEnvironment pkgs.libev];
+        # buildInputs = scope';
 
         shellHook = ''
           export OCAMLPATH="${ocamlEnvironment}/lib/ocaml/${ocaml.version}/site-lib"
+          export OCAMLPATH="${ocamlEnvironment}/lib/ocaml/5.1.0/site-lib"
           echo    $OCAMLPATH
           export NIX_PATH=nixpkgs=${nixpkgs}:$NIX_PATH
         '';
       };
 
 
-    a=(opam-nix.lib.x86_64-linux.buildOpamProject' { } ./. { ocaml-base-compiler = "*"; }).myPro;
-    packages.x86_64-linux.default = (opam-nix.lib.x86_64-linux.buildOpamProject'  {} ./. { ocaml-base-compiler = "*"; }).myPro;
-    # packages.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; ocamlPackages.buildDunePackage rec {
-    #   pname = "myPro";
-    #   version = "0.1.0";
-    #   duneVersion = "3";
-    #   src = ./.;
-    #
-    #   buildInputs = [
-    #     ocamlPackages.core
-    #     ocamlPackages.dream
-    #     ocamlPackages.tyxml
-    #     ocamlPackages.alcotest
-    #     ocamlPackages.ppx_inline_test
-    #   ];
-    #
-    #   #useDuneConfig = true; # Use this to read dependencies from .opam file
-    #   doCheck = true;
-    #   checkTarget = "test";
-    #
-    #   #preBuild = ''dune build myPro.opam '';
-    # };
+    # a = (opam-nix.lib.x86_64-linux.buildOpamProject' { } ./. { ocaml-base-compiler = "*"; });
+    b = (nixpkgs.legacyPackages.x86_64-linux.lib.getAttr a);
+    aa = (opam-nix.lib.x86_64-linux.buildOpamProject { } ./. { ocaml-base-compiler = "*"; });
+    aaa = a.overrideScope' { };
+    packages.x86_64-linux.default = (opam-nix.lib.x86_64-linux.buildOpamProject' { } ./. { ocaml-base-compiler = "*"; }).myPro;
     apps.x86_64-linux.default = { type = "app"; program = "${packages.x86_64-linux.default}/bin/main"; };
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
   };
