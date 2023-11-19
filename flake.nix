e26dacd41751ed24999f0b28cc3a6d0a568d548c
marius <marnyg@proton.me>
2023-11-19 14:54:11 +0000

    final fix of flake

--- flake.nix
+++ flake.nix
@@ -6,12 +6,18 @@
 
   outputs = { self, nixpkgs, opam-nix }:
     let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
       myPro = (opam-nix.lib.x86_64-linux.buildDuneProject { } "myPro" ./. { ocaml-base-compiler = "*"; }).myPro;
+      myShell = pkgs.mkShell {
+        buildInputs =  [ myPro pkgs.nixd pkgs.rnix-lsp ];
+        LSP_SERVERS = "ocamllsp, rnix, nixd";
+      };
     in
     {
       packages.x86_64-linux.default = myPro;
       apps.x86_64-linux.default = { type = "app"; program = "${myPro}/bin/main"; };
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      formatter.x86_64-linux = pkgs.nixpkgs-fmt;
+      devShells.x86_64-linux.default = myShell;
     };
 }
 
